dfff1575e813ab6c30760e492ede4c012915cdc9
LeeDroid- <leedroid.lb@gmail.com>
2011-09-19 20:00:25 +0100

    Modify KGSL Ringbuffer power managment (faux123)

--- drivers/video/msm_8x60/gpu/kgsl_adreno220/kgsl_yamato.c
+++ drivers/video/msm_8x60/gpu/kgsl_adreno220/kgsl_yamato.c
@@ -783,6 +783,7 @@ static int kgsl_yamato_start(struct kgsl_device *device, unsigned int init_ram)
 	int status = -EINVAL;
 	struct kgsl_yamato_device *yamato_device = KGSL_YAMATO_DEVICE(device);
 	int init_reftimestamp = 0x7fffffff;
+	unsigned int override1, override2, i;
 
 	KGSL_DRV_VDBG("enter (device=%p)\n", device);
 
@@ -852,8 +853,21 @@ static int kgsl_yamato_start(struct kgsl_device *device, unsigned int init_ram)
 	kgsl_yamato_regwrite(device, REG_RBBM_PM_OVERRIDE1, 0);
 	if (device->chip_id != KGSL_CHIPID_LEIA_REV470)
 		kgsl_yamato_regwrite(device, REG_RBBM_PM_OVERRIDE2, 0);
-	else
-		kgsl_yamato_regwrite(device, REG_RBBM_PM_OVERRIDE2, 0x80);
+	else{
+		i = 3; /*try writing override1 & 2, three times.*/
+		while(i){
+			kgsl_yamato_regwrite(device, REG_RBBM_PM_OVERRIDE1, 0x7BFFFFFA);
+			kgsl_yamato_regread(device, REG_RBBM_PM_OVERRIDE1, &override1);
+			kgsl_yamato_regwrite(device, REG_RBBM_PM_OVERRIDE2, 0x000001F4);
+			kgsl_yamato_regread(device, REG_RBBM_PM_OVERRIDE2, &override2);
+			if (((override1 & 0x7BFFFFFA) == 0x7BFFFFFA) &&
+				((override2 & 0x000001F4) == 0x000001F4))
+				break;
+			KGSL_DRV_ERR("OVERRIDE1 = 0x%x, OVERRIDE2 = 0x%x !!\n",
+								override1, override2);
+			i--;
+		}
+        }
 
 	kgsl_sharedmem_set(&device->memstore, 0, 0,
 			   device->memstore.size);
